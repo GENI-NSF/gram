e1acbd9b262c402e80850c04968f869d6d3e6485
Marshall Brinn <mbrinn@bbn.com>
2012-12-11 10:58:44 -0500

    Delete ports before deleting net

--- src/geni/am/gram/cleanup.py
+++ src/geni/am/gram/cleanup.py
@@ -9,6 +9,27 @@ if len(sys.argv) < 2 :
     print 'Usage: cleanup <slicename>...<slicename>'
     sys.exit(2)
 
+# Use port-show to determine if a given port is a network_router port 
+# (and thus shouldn't be deleted)
+def determine_network_router_interface(port):
+    port_show_cmd = "quantum port-show %s" % port
+    try:
+        port_show_output = open_stack_interface._execCommand(port_show_cmd)
+    except Exception:
+        # Sometimes the port doesn't exist: 
+        # it was deleted by a previous quantum delete command
+        return False
+#            print "PSO = " + port_show_output
+    port_show_output_lines = port_show_output.split('\n')
+    port_is_network_router_interface = False
+    for k in range(3, len(port_show_output_lines)-2):
+        port_show_columns = port_show_output_lines[k].split('|')
+        if port_show_columns[1].strip() == 'device_owner' and \
+                port_show_columns[2].strip() == 'network:router_interface':
+            port_is_network_router_interface = True
+            break
+    return port_is_network_router_interface
+
 tenant_pwd = config.tenant_admin_pwd
 
 for i in range(1, len(sys.argv)) :
@@ -62,15 +83,50 @@ for i in range(1, len(sys.argv)) :
         open_stack_interface._execCommand(cmd_string)
 
 
+    # Find all ports (we'll be deleting the ones 
+    # on the subnet of the network of this tenant
+    ports_cmd_string = 'quantum port-list'
+    print ports_cmd_string
+    ports_output = open_stack_interface._execCommand(ports_cmd_string)
+#    print "PORTS = " + ports_output
+    ports_output_lines = ports_output.split('\n')
+
     # Find the networks owned by this tenant
+    # And delete corresponding ports (that aren't owned by network:router_interface
     cmd_string = 'quantum net-list -- --tenant_id %s' % tenant_uuid
     print cmd_string
-    output = open_stack_interface._execCommand(cmd_string)
+    net_list_output = open_stack_interface._execCommand(cmd_string)
+#    print "NETS = " + net_list_output
+    net_list_output_lines = net_list_output.split('\n')
+    # And delete ports then subnet
+    for i in range(3, len(net_list_output_lines) - 2):
+        columns = net_list_output_lines[i].split('|')
+        subnet = columns[3].strip()
+#        print "SN = " + subnet
+        for j in range(3, len(ports_output_lines) - 2):
+            port_columns = ports_output_lines[j].split('|')
+            port = port_columns[1]
+            fixed_ips = port_columns[4]
+            fixed_ips_pieces = fixed_ips.split('"')
+            if len(fixed_ips_pieces) < 3:
+                break
+            port_sn = fixed_ips_pieces[3].strip()
+#            print "PORT = " + port + " FIPS = " + fixed_ips + " SN = " + port_sn
+            port_is_network_router_interface = determine_network_router_interface(port)
+            if port_sn == subnet and not port_is_network_router_interface:
+#                print "MATCH"
+                port_delete_cmd = "quantum port-delete %s" % port
+                print port_delete_cmd
+                try:
+                    port_delete_output = open_stack_interface._execCommand(port_delete_cmd)
+                except Exception:
+                    # Sometimes deleting a port deletes a partner as well, so the list is stale
+                    print "Error deleting port: " + port_delete_cmd
+                    
 
     # Delete the networks 
-    output_lines = output.split('\n')
-    for i in range(3, len(output_lines) - 2) :
-        columns = output_lines[i].split('|')
+    for i in range(3, len(net_list_output_lines) - 2) :
+        columns = net_list_output_lines[i].split('|')
         net_uuid = columns[1]
         cmd_string = 'quantum net-delete %s' % net_uuid
         print cmd_string
