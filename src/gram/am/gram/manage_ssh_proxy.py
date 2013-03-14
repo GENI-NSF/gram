d91cde688869506562c266957182e81e6e707b2d
John Shackleton <jshackle@bbn.com>
2013-03-14 15:09:29 -0400

    Updated SSH proxy support so that port management is performed by the AM and not the proxy (binary) utility.
    Former-commit-id: 9b57b8769a9e594f4ba1c26ebafc3bf6cbc62cf2

--- src/gram/am/gram/manage_ssh_proxy.py
+++ src/gram/am/gram/manage_ssh_proxy.py
@@ -26,21 +26,41 @@ import config
 import string
 import struct
 import fcntl
-
+import os
 
 def _acquireReadLock() :
         lockFile = None
 	try:
             lockFile = open(config.port_table_lock_file, 'r')
 	except IOError:
-            config.logger.error("Unable to open file %s" % config.port_table_lock_file)
+            config.logger.error("Unable to open file %s for reading" % config.port_table_lock_file)
             return None
 
         lockdata = struct.pack('hhllhh', fcntl.F_RDLCK, 0, 0, 0, 0, 0)
         try :
             fcntl.fcntl(lockFile.fileno(), fcntl.F_SETLKW, lockdata)
         except :
-            config.logger.error("Unable to lock file %s" % config.port_table_lock_file)
+            lockFile.close()
+            config.logger.error("Unable to lock file %s for reading" % config.port_table_lock_file)
+            return None
+
+        return lockFile
+
+
+def _acquireWriteLock() :
+        lockFile = None
+	try:
+            lockFile = open(config.port_table_lock_file, 'w')
+	except IOError:
+            config.logger.error("Unable to open file %s for writing" % config.port_table_lock_file)
+            return None
+
+        lockdata = struct.pack('hhllhh', fcntl.F_WRLCK, 0, 0, 0, 0, 0)
+        try :
+            fcntl.fcntl(lockFile.fileno(), fcntl.F_SETLKW, lockdata)
+        except :
+            lockFile.close()
+            config.logger.error("Unable to lock file %s for writing" % config.port_table_lock_file)
             return None
 
         return lockFile
@@ -52,37 +72,39 @@ def _releaseLock(lockFile) :
             fcntl.fcntl(lockFile.fileno(), fcntl.F_SETLK, lockdata)
         except :
             config.logger.error("Unable to release lock file %s" % config.port_table_lock_file)
-            return None
 
         lockFile.close()
     
 
-def _getPortFromTable(addr) :
+def _getPortFromTable(addr, delete) :
 	"""
 	1) Open port file
 	2) Iterate through entries
 	3) Find matching address
 	"""
         # Acquire file lock on SSH port table
-        filelock = _acquireReadLock()
-        if filelock == None :
+        lockfile = _acquireReadLock()
+        if lockfile == None :
             return 0
 
         # Attempt to open the port table file for reading
         portLines = []
-        try:
+        try :
             scriptFile = open(config.port_table_file, 'r')
             portLines = scriptFile.readlines()
             scriptFile.close()
-	except IOError:
+        except :
+            _releaseLock(lockfile)
             config.logger.error("Unable to open file %s for reading" % config.port_table_file)
             return 0
 
         # Release the SSH port table file lock
-        _releaseLock(filelock)
+        _releaseLock(lockfile)
 
         # If port table exists than iterate through sorted entries and find an available port
 	portNumber = 0
+        index = 0
+        table_index = -1
 	for portLine in portLines :
 		# Entries are of the format { address \t port }
 		tokens = string.split(portLine)
@@ -90,10 +112,135 @@ def _getPortFromTable(addr) :
 			# Compare the port number to the current port counter
 			# If the port is available then stop the search, otherwise go to the next port
 			if addr == tokens[0] :
-				return int(tokens[1])
+				portNumber = int(tokens[1])
+                                table_index = index
+                index += 1
 
-	config.logger.error("Unable to find the address %s in port table file %s" % (addr % config.port_table_file))
-	return 0
+        # Now delete the line in the port file if necessary
+        if delete and table_index >= 0 :
+            lockfile = _acquireWriteLock()
+	    if lockfile == None :
+                return portNumber
+
+            try :
+                scriptFile = open(config.port_table_file, 'w')
+            except :
+                _releaseLock(lockfile)
+                config.logger.error("Unable to open file %s for reading" % config.port_table_file)
+                return portNumber
+
+            # Write the contents of the port table
+            index = 0
+            for portLine in portLines :
+                if index != table_index :
+                    scriptFile.write(portLine)
+                index += 1
+
+            # Release the file lock and close the file
+            scriptFile.close()
+            _releaseLock(lockfile)
+
+	return portNumber
+
+
+def _updatePortTable(addr) :
+    # Attempt to open the port table file for reading
+    portLines = []
+    scriptFile = None
+    table_exists = False
+    if os.path.isfile(config.port_table_file) :
+        # Acquire file lock on SSH port table
+        table_exists = True
+        lockfile = _acquireReadLock()
+        if lockfile == None :
+            return 0
+
+        # Attempt to open the port table file for reading
+        try :
+            scriptFile = open(config.port_table_file, 'r')
+            portLines = scriptFile.readlines()
+            scriptFile.close()
+        except :
+            _releaseLock(lockfile)
+            config.logger.error("Unable to open file %s for reading" % config.port_table_file)
+            return 0
+
+        # Release the SSH port table file lock
+        _releaseLock(lockfile)
+
+    # Iterate through the port table
+    duplicate = False
+    index = 0
+    portCounter = config.ssh_proxy_start_port
+    portStr = '%d' % portCounter
+    insert_index = -1
+    for portLine in portLines :
+	# Entries are of the format { address \t port }
+	tokens = string.split(portLine)
+	if len(tokens) > 1 :
+            # Compare the port number to the current port counter
+	    # If the port is available then stop the search, otherwise go to the next port
+            portNumber = int(tokens[1])
+	    if addr == tokens[0] :
+                duplicate = True
+            elif insert_index < 0 :
+                if portCounter == portNumber :
+                    portCounter += 1
+		    portStr = '%d' % portCounter
+		else :
+                    insert_index = index
+
+        # Increment the table entry index
+        index += 1
+
+    # Exit early if a duplicate entry is found
+    if duplicate :
+        config.logger.error("Duplicate address %s in port table file" % addr)
+        return 0
+
+    # Exit early if the port table has reached its maximum number of ports
+    if config.ssh_proxy_end_port - config.ssh_proxy_start_port + 1 <= insert_index :
+        config.logger.error("Maximum ports reached in SSH proxy port table")
+        return 0
+
+    # Insert a new line into the port table
+    tab_char = '\t'
+    newEntry = '%s%s%s%s' % (addr, '\t', portStr, '\n')
+    if insert_index < 0 :
+        portLines.append(newEntry)
+    else :
+        portLines.insert(insert_index, newEntry)
+
+    # Ensure proper file permissions if this the port table is getting created
+    if not table_exists :
+        open(config.port_table_lock_file, 'w').close()
+        os.chmod(config.port_table_lock_file, 0666)
+        open(config.port_table_file, 'w').close()
+        os.chmod(config.port_table_file, 0666)
+
+    # Acquire a write lock on the port table file
+    lockfile = _acquireWriteLock()
+    if lockfile == None :
+        return 0
+
+    try :
+        scriptFile = open(config.port_table_file, 'w')
+    except :
+        _releaseLock(lockfile)
+        config.logger.error("Unable to open file %s for writing" % config.port_table_file)
+        return 0
+
+    # Write the contents of the port table
+    for portLine in portLines :
+        scriptFile.write(portLine)
+
+    scriptFile.close()
+
+    # Release the file lock and close the file
+    _releaseLock(lockfile)
+
+    # All good- return the assigned port number
+    return portCounter
 
 
 def _addNewProxy(addr) :
@@ -101,8 +248,13 @@ def _addNewProxy(addr) :
     Access the GRAM SSH proxy daemon and create a new proxy with the given address
     """
 
+    portNumber = _updatePortTable(addr)
+    if portNumber == 0 :
+        return 0
+
     cmd_string = '%s ' % config.ssh_proxy_exe
     cmd_string = cmd_string + '-m C -a %s ' % addr
+    cmd_string = cmd_string + ' -p %d ' % portNumber
 
     try :
         open_stack_interface._execCommand(cmd_string)
@@ -110,7 +262,7 @@ def _addNewProxy(addr) :
         config.logger.error("Unable to create SSH proxy for address  %s" % addr)
         return 0
 
-    return _getPortFromTable(addr)
+    return portNumber
 
 
 def _removeProxy(addr) :
@@ -118,10 +270,13 @@ def _removeProxy(addr) :
     Access the GRAM SSH proxy daemon and delete the with the given address
     """
 
-    cmd_string = '%s ' % config.ssh_proxy_exe
-    cmd_string = cmd_string + '-m D -a %s ' % addr
+    portNumber = _getPortFromTable(addr, True)
+    if portNumber > 0 :
+        cmd_string = '%s ' % config.ssh_proxy_exe
+        cmd_string = cmd_string + '-m D -a %s ' % addr
+        cmd_string = cmd_string + '-p %d ' % portNumber
 
-    try:
-        open_stack_interface._execCommand(cmd_string)
-    except :
-        config.logger.error("Address %s not present in SSH proxy" % addr)
+        try:
+            open_stack_interface._execCommand(cmd_string)
+        except :
+            config.logger.error("Address %s not present in SSH proxy" % addr)
