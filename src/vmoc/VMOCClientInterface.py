bc2b62ca0d5ec0aaff80b5401f6d2ff1d37d07ea
Marshall Brinn <mbrinn@bbn.com>
2013-04-11 07:37:57 -0400

    Support periodic ping of VMOC to tell when it has changed, pus support clear operation on GRAN startup, plus fix but in unregister (config, not slice_id)
    Former-commit-id: d6b6459c2149b297ed53c64772633125274f7457

--- src/vmoc/VMOCClientInterface.py
+++ src/vmoc/VMOCClientInterface.py
@@ -55,12 +55,23 @@ class VMOCClientInterface(threading.Thread):
     # How often to try to reconnect to VMOC?
     _ping_interval = 5
 
+    # What is the PID of the VMOC process we've been talking to?
+    _vmoc_pid = None
+
+    # Has the VMOC restarted since last check (the PID is different)
+    _vmoc_restarted = False
+
     def __init__(self):
 
         threading.Thread.__init__(self)
         self._running = False
         self._vmoc_is_up = False
         self._vmoc_host = 'localhost'
+
+        # First time we run, tell the client to 
+        # erase any previous slice info
+        VMOCClientInterface.clear()
+
 
     # Thread loop
     # Maintain a socket connection
@@ -74,33 +85,55 @@ class VMOCClientInterface(threading.Thread):
 
       while self._running:
 
+          latest_vmoc_pid = None
+
+#          config.logger.info("VMOC_IS_UP : "+ str(self._vmoc_is_up));
+
           # Is VMOC down? If so, flush queue and fill with all configs
           try:
-              sock = self.connectionToVMOC()
+              sock = self.connectToVMOC()
+#              config.logger.info("SOCK = " + str(sock));
               if sock:
                   sock.send('ping')
+                  latest_vmoc_pid = sock.recv(1024)
+#                  config.logger.info("VMOC_PID /" + latest_vmoc_pid + "/ " + str(len(latest_vmoc_pid)))
+                  if latest_vmoc_pid != self._vmoc_pid:
+                      config.logger.info("VMOC_PID changed from %s to %s" % \
+                                             (self._vmoc_pid, latest_vmoc_pid))
+                      self._vmoc_restarted = True
+                      self._vmoc_pid = latest_vmoc_pid
                   sock.close()
+#                  config.logger.info("After close");
                   self._vmoc_is_up =  True
               else:
                   if self._vmoc_is_up:
                       config.logger.info("VMOC went down ")
                       self._vmoc_is_up = False
+
           except Exception as e:
+              config.logger.info("Exception (1) : " + str(e))
               if self._vmoc_is_up:
                   config.logger.info("VMOC went down " + str(e))
                   self._vmoc_is_up  = False
 
+#          config.logger.info("UP = " + str(self._vmoc_is_up))
+#          config.logger.info("RESTARTED = " + str(self._vmoc_restarted))
+
                   
-          # Connection is down. Put all configs into queue
-          if not self._vmoc_is_up:
+          # Connection is down or recently reset. 
+          # Put all configs into queue
+          if not self._vmoc_is_up or self._vmoc_restarted:
               with VMOCClientInterface._lock:
                   slice_configs = \
                       VMOCClientInterface._configs_by_slice.values()
                   VMOCClientInterface._pending_queue = []
-#                  config.logger.info("Disconnected from VMOC: " + 
-#                                     "restoring slice configs")
+                  config.logger.info("Disconnected from VMOC: " + 
+                                     "restoring slice configs")
                   for slice_config in slice_configs:
                       VMOCClientInterface.register(slice_config)
+                  self._vmoc_restarted = False
+
+
 
   # If connected, try to send all messages
   # If we fail sending any message, connection is closed
@@ -109,8 +142,8 @@ class VMOCClientInterface(threading.Thread):
                   entry = VMOCClientInterface._pending_queue[0]
                   msg = entry['msg']
                   try:
+                      sock = self.connectToVMOC()
 #                      print " Trying to send " + str(entry)
-                      sock = self.connectionToVMOC()
                       if sock:
                           sock.send(msg)
                           sock.close()
@@ -120,15 +153,14 @@ class VMOCClientInterface(threading.Thread):
                       else:
                           self._vmoc_is_up = False
                   except Exception as e:
-                      print "Exception "  + str(e)
-                      sock.close()
+                      print "Exception (2) "  + str(e)
                       self._vmoc_is_up = False
 
           # Wake up every N seconds
           time.sleep(VMOCClientInterface._ping_interval)
 
-    # Return a conenction to VMOC Management interface
-    def connectionToVMOC(self):
+    # Establish a connection to VMOC Management interface
+    def connectToVMOC(self):
         sock = None
         addr = (self._vmoc_host, config.vmoc_interface_port)
         try:
@@ -152,11 +184,16 @@ class VMOCClientInterface(threading.Thread):
 
     @staticmethod
     def create_queue_entry(slice_id, slice_config, register):
+        if not slice_config:
+            print "SLICE_ID = " + str(slice_id)
+            print "CONFIGS_BY_SLICE = " + str(VMOCClientInterface._instance._configs_by_slice)
+            slice_config = VMOCClientInterface._instance._configs_by_slice[slice_id];
         if slice_config: slice_id = slice_config.getSliceID()
         if register:
             msg = 'register %s' % json.dumps(slice_config.__attr__())
         else:
-            msg = 'unregister %s' % slice_id
+#            msg = 'unregister %s' % slice_id
+            msg = 'unregister %s' % json.dumps(slice_config.__attr__())
         entry = {'slice_id': slice_id, 'msg':msg}
         return entry
 
@@ -172,15 +209,23 @@ class VMOCClientInterface(threading.Thread):
 #        VMOCClientInterface.dumpQueue()
 
     @staticmethod
-    def unregister(slice_id):
+    def unregister(slice_config):
         with VMOCClientInterface._lock:
+            slice_id = slice_config.getSliceID()
             VMOCClientInterface._pending_queue = \
                 [msg for msg in VMOCClientInterface._pending_queue if msg['slice_id'] != slice_id]
             queue_entry = \
-                VMOCClientInterface.create_queue_entry(slice_id, None, False)
+                VMOCClientInterface.create_queue_entry(slice_id, slice_config, False)
             VMOCClientInterface._pending_queue.append(queue_entry)
             if VMOCClientInterface._configs_by_slice.has_key(slice_id):
                 del VMOCClientInterface._configs_by_slice[slice_id]
+#        VMOCClientInterface.dumpQueue()
+
+    @staticmethod
+    def clear():
+        with VMOCClientInterface._lock:
+            clear_entry = {'slice_id': None, 'msg': "clear"}
+            VMOCClientInterface._pending_queue.append(clear_entry)
 #        VMOCClientInterface.dumpQueue()
 
     @staticmethod
