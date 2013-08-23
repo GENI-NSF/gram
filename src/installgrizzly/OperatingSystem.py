06331b84b402bb5665e6f5e6e41cc39681fab4ee
dabideen <dabideen@bbn.com>
2013-08-23 08:52:42 -0400

    Sets up /etc/hosts
    Former-commit-id: 75fbd7958ddd9b8af4e13536acbe386e1422b8b8

--- src/installgrizzly/OperatingSystem.py
+++ src/installgrizzly/OperatingSystem.py
@@ -113,6 +113,10 @@ class OperatingSystem(GenericInstaller):
         # configure ~/.gcf/gcf_config
         self.sed("s/base_name=geni.*/base_name=geni\/\/" + config.service_token + "\/\/gcf","/home/gram/.gcf/gcf_config")
         self.sed("s/host=.*/host=" + config.control_host + "/","\/\/gcf","/home/gram/.gcf/gcf_config")
+        self.appendToFile(config.control_host + " " + config.control_host_addr, "/etc/hosts")
+        nodes = config.compute_hosts
+        for node in nodes:
+            self.appendToFile(node + " " + nodes[node],"/etc/hosts")
 
 
     # Return a list of command strings for uninstalling this component
