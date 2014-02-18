c16492af1c3e1bdbf2e1d65090e4ad8fea92be44
dabideen <dabideen@bbn.com>
2014-02-18 11:59:58 -0500

    Fixed the am_type return of the v2 getVersion call
    Former-commit-id: 6fedc2f0196bf509b246a989a5c490472294c282

--- src/gram/am/gram_am2.py
+++ src/gram/am/gram_am2.py
@@ -58,6 +58,7 @@ class GramReferenceAggregateManager(ReferenceAggregateManager):
         v2_url = "https://%s:%d" % (hostname, config.gram_am_v2_port)
         geni_api_versions = {'2' : v2_url, '3' : v3_url}
         result['value']['geni_api_versions'] = geni_api_versions
+        result['code']['am_type'] = 'GRAM'
         return result
 
     def ListResources(self, credentials, options):
