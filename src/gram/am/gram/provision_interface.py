28ba8f9bafe9de25b36b8fa8f47c6bb5ba007852
Mellondawg <jmelloni@umass.edu>
2017-06-28 17:19:22 -0400

    Added support for multiple users.

--- src/gram/am/gram/provision_interface.py
+++ src/gram/am/gram/provision_interface.py
@@ -109,139 +109,146 @@ def provisionResources(geni_slice, slivers, users, gram_manager) :
                                   users, gram_manager, geni_slice)
     return create_return
 
+# To be renamed to pis
 def _createAllVMs(vms_to_be_provisioned, users, gram_manager, geni_slice):
     
-    vm_len = len(vms_to_be_provisioned)
     #config.logger.info('vms_to_be_provisioned = "%s" len of vms = "%s" users = "%s" gram_manager = "%s" geni_slice = "%s" ' % \
 #	(vms_to_be_provisioned, vm_len, users, gram_manager, geni_slice)) 
-	
-    scriptList = []
-    scriptList2 = []
-    users2 = []    
-    #users2 = [{'keys':['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCglU3HMVOuUUroW+RsQ8e2J5yY8JjAXDkglHzDs7c8JMP2SKGlMO6F1ADh4LHZqjj56QZP+VCIxex8UB768tca2Q9h06DxcLjGCOVHhgbi1m6v3+9IimtWQhmRygXD6yxEgRv4IFcaQpl08A3ZGBYQRG4CvnkomPThWlsYZfJwO0F37hN9noCA1DtBml1TEwFQ9I2wHv8o16oilX7iU34BWs6JTOD92BB+aXcB757Ne/ahyG1Kpm+QlRuCIOlZSzk6TdX22YSDI3GSrClIFX7fO9ka7RBgxm2/ITBHi5bDXKi9/N1kvUryiPC+ltRA2nFnk7AXRBzBr3e2XC6LBJsPoEu9JMPv5A/NzN2DRAKeQ72AwkgR3IrUnAjI+K3X013ufk67X5Ik5/wccpnLAdJQi7xDbup4gUKMyvuM/rnJRgLN90jz52T3fZegLgP73f3OfXYIgRoEN2as32+TV8cSIF2btWNOTG3X0CfN5VoH73Bka62Q4OwEeQ5XOYwE26/YvWtF+JU8vQKyRehB/ejUE2/tC7jNwPfDfTRfFjh2k5u1/7O1/pAGqduk0qUsRuoZ+KOzJFHPj8VX6qgi/ZxwDSEC7bqBm9UCJ5A03U8Npj177tQ+GvO/E/mAwAmYb+kZlf+RtmFJ+R9j7yB57XscrHs6/Tdfwxcnwnq2XLhZpQ== jmelloni@umass.edu'], 'urn': 'urn:publicid:IDN+geni:boscontroller:gcf+user+jmelloni2'}]    
 
+    # List that will hold one script for each user. Script will establish ssh access	
+    scriptList = []
+
+    # Parse manifest rspec to get currently allocated pi name
     mani_rspec = geni_slice._manifest_rspec
     temp = mani_rspec.split('"')
     pi_name = temp[13]
- 
-    for user in users:
-	if user["urn"] == "urn:publicid:IDN+geni:boscontroller:gcf+user+gramuser":
-	    config.logger.info("skipping gramuser")
-	else :
-	    users2.append(user)
-	
-    for user in users2:
-	scriptName=_generateAccount(user)
-	scriptList.append(scriptName)
-	scriptName2=_generateClean(user)
-	scriptList2.append(scriptName2)
 
-    config.logger.info('scriptList = "%s" ' % (scriptList))
-    config.logger.info('scriptList2 = "%s" ' % (scriptList2))
+    # retrieve proper ip address based on pi name
+    pi_list = config.rpi_metadata
+    pidata = pi_list[pi_name]
+    public_ipv4 = pidata['public_ipv4']
+    #config.logger.info("public_ipv4 = %s" % public_ipv4)
     
-    HOST = "pi@128.89.91.174"
-    COMMAND = "sudo ./temp.sh"
-    script = scriptList[0]
-    script2 = scriptList2[0]
-    config.logger.info('SCRIPT NAME = %s' % (script))
-    subprocess.call(['chmod', '+x', script])
-    subprocess.call(['chmod', '+x', script2]) 
-    subprocess.call(['scp', script, '%s:/home/pi/temp.sh' % HOST ])
-    subprocess.call(['scp', script2, '%s:/home/pi/clean.sh' % HOST ])
+    HOST = "pi@%s" % public_ipv4
+    #config.logger.info("host = %s" % HOST) 
+ 
+    # Generate one init script and one clean script that will handle all users
+    initScript=_generateAccount(users)
+    cleanScript=_generateClean(users)
 
-    ssh = subprocess.Popen(["ssh", "%s" % HOST, "sudo", "./temp.sh"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = ssh.stdout.readlines()
-    if result == []:
-	for vm in vms_to_be_provisioned:
-	    vm.setAllocationState(constants.provisioned)
-	    vm.setOperationalState(constants.ready)
-	error = ssh.stderr.readlines()
-	config.logger.info("Error: %s" % error)
-    else:
-	config.logger.info("RESULT IS HERE: %s" % result)
+    # config.logger.info('scriptList = "%s" ' % (scriptList))
+    # config.logger.info('scriptList2 = "%s" ' % (scriptList2))
+    
+    
+    #HOST = "pi@128.89.91.174"
+    
+    # Copy over and execute each account generation script
+    subprocess.call(['chmod', '+x', initScript])
+    subprocess.call(['scp', initScript, '%s:/home/pi/temp/initScript.sh' % HOST])
+    ssh = subprocess.Popen(["ssh", "%s" % HOST, "sudo", "./temp/initScript.sh"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Change the state of the "vm"	
+    for vm in vms_to_be_provisioned:
+        vm.setAllocationState(constants.provisioned)
+	vm.setOperationalState(constants.ready)
+
+    # Copy over and execute the clean-up script
+    subprocess.call(['chmod', '+x', cleanScript])
+    subprocess.call(['scp', cleanScript, '%s:/home/pi/temp/clean.sh' % HOST])
 
     return None
 
-def _generateAccount(user) :
-    """ Generate a script that creates a new user account with SSH authentication
+def _generateAccount(users) :
+    """ Generate a script that creates a new user account with SSH authentication, one script for all users
     """
 
     userName = ""
     scriptFilename = ""
     publicKeys = []
 
-    # Go through every user and get the user's name and ssh public key
-    for key in user.keys() :
-        # Found a user, there should only be one of these per key in 'user'
-        if key == "urn" :
-            userName = user[key]
-            userName = userName.split("+")[-1]
+    # Open the script file for writing
+    tempscriptfile = tempfile.NamedTemporaryFile(delete=False)
+    scriptFilename = '%s' % tempscriptfile.name
+    try:
+        scriptFile = open(scriptFilename, 'w')
+    except IOError:
+        config.logger.error("Failed to open file that creates metadata: %s" % scriptFilename)
+        return ""
 
-        # Found a new public key list, store all the public keys
-        elif key == "keys" :
-            for value in user[key] :
-                publicKeys.append(value)
+    # Use sh script
+    scriptFile.write('#!/bin/sh \n')
 
-    # Only install the user account if there is a user to install
-    if userName != "" :
+    for user in users:
+        # Go through every user and get the user's name and ssh public key(s)
+        for key in user.keys() :
+            # Found a user, there should only be one of these per key in 'user'
+            if key == "urn" :
+	        if user[key] == "urn:publicid:IDN+geni:boscontroller:gcf+user+gramuser" :
+		    pass
+	        else :
+		    userName = user[key]
+		    userName = userName.split("+")[-1]
 
-        # Open the script file for writing
-        tempscriptfile = tempfile.NamedTemporaryFile(delete=False)
-        scriptFilename = '%s' % tempscriptfile.name
-        try:
-            scriptFile = open(scriptFilename, 'w')
-        except IOError:
-            config.logger.error("Failed to open file that creates metadata: %s" % scriptFilename)
-            return ""
+            # Found a new public key list, store all the public keys for this user
+            elif key == "keys" :
+                for value in user[key] :
+                    publicKeys.append(value)
 
-        # Use sh script
-        scriptFile.write('#!/bin/sh \n')
+	# This part of the script is for each user
+        # Only install the user account if there is a user to install
+        if userName != "" :
 
-        # Create account with default shell
-        scriptFile.write('useradd -c "%s user" -s /bin/bash -m %s\n' % (userName, userName))
+            # Create account with default shell
+            scriptFile.write('useradd -c "%s user" -s /bin/bash -m %s\n' % (userName, userName))
 
-        # Configure SSH authentication for new user
-        scriptFile.write('mkdir ~%s/.ssh \n' % userName)
-        scriptFile.write('chmod 755 ~%s/.ssh \n' % userName)
+            # Configure SSH authentication for new user
+            scriptFile.write('mkdir ~%s/.ssh \n' % userName)
+            scriptFile.write('chmod 755 ~%s/.ssh \n' % userName)
 
-        # Write the set of user public keys
-        for str in publicKeys :
-            newstr = str.replace("\n", "")
-            scriptFile.write('echo \'%s\' >> ~%s/.ssh/authorized_keys \n' % (newstr, userName))
+            # Write the set of user public keys
+            for str in publicKeys :
+                newstr = str.replace("\n", "")
+                scriptFile.write('echo \'%s\' >> ~%s/.ssh/authorized_keys \n' % (newstr, userName))
 
-        # Make ssh authorized keys available
-        # Note that in the chown command below requires the escape character printed before the colon (":")
-        #   because non-VASL characters in the cloud-config syntax require a qualifier in order to be properly parsed
-        scriptFile.write('chmod 644 ~%s/.ssh/authorized_keys \n' % userName)
-        scriptFile.write('chown -R %s\\:%s ~%s/.ssh \n' % (userName, userName, userName))
+            # Make ssh authorized keys available
+            # Note that in the chown command below requires the escape character printed before the colon (":")
+            #   because non-VASL characters in the cloud-config syntax require a qualifier in order to be properly parsed
+            scriptFile.write('chmod 644 ~%s/.ssh/authorized_keys \n' % userName)
+            scriptFile.write('chown -R %s\\:%s ~%s/.ssh \n' % (userName, userName, userName))
 
-        # Add users to sudoers list
-        scriptFile.write('echo "%s  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers' % userName)
+            # Add users to sudoers list
+            scriptFile.write('echo "%s  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers \n' % userName)
 
-        scriptFile.close()
+	    # Reset the publicKeys var for the next user
+	    publicKeys = []
+
+    scriptFile.close()
 
     return scriptFilename
 
-def _generateClean(user) :
+def _generateClean(users) :
     """ Generate a script that removes the user and deletes their home directory and SSH access
     """
 
     userName = ""
+    userNames = []
     scriptFilename = ""
 
-    # Go through every user and get the user's name and ssh public key
-    for key in user.keys() :
-        # Found a user, there should only be one of these per key in 'user'
-        if key == "urn" :
-            userName = user[key]
-            userName = userName.split("+")[-1]
+    # Create a list of all usernames except for gramuser
+    for user in users :
+        for key in user.keys() :
+            if key == "urn" :
+	        if user[key] == "urn:publicid:IDN+geni:boscontroller:gcf+user+gramuser" :
+		    pass
+	        else :
+		    userName = user[key]
+		    userName = userName.split("+")[-1]
+		    userNames.append(userName)
 
-    # Only install the user account if there is a user to install
-    if userName != "" :
-
-        # Open the script file for writing
-        tempscriptfile = tempfile.NamedTemporaryFile(delete=False)
-        scriptFilename = '%s' % tempscriptfile.name
+    if userNames != [] :
+	# Open the script file for writing
+	tempscriptfile = tempfile.NamedTemporaryFile(delete=False)
+	scriptFilename = '%s' % tempscriptfile.name
         try:
             scriptFile = open(scriptFilename, 'w')
         except IOError:
@@ -251,11 +258,13 @@ def _generateClean(user) :
         # Use sh script
         scriptFile.write('#!/bin/sh \n')
 
-        # Delete user and home directory
-        scriptFile.write('deluser --remove-home %s \n' % userName)
-
-        # Remove users from sudoers list
-        scriptFile.write('sed -i "s/%s  ALL=(ALL) NOPASSWD:ALL//g" /etc/sudoers' % userName)
+        for name in userNames:
+       	    # Only install the user account if there is a user to install
+	    if name != "" :
+        	# Delete user and home directory
+        	scriptFile.write('deluser --remove-home %s \n' % name)
+        	# Remove users from sudoers list
+        	scriptFile.write('sed -i "s/%s  ALL=(ALL) NOPASSWD:ALL//g" /etc/sudoers \n' % name)
 
         scriptFile.close()
 
@@ -274,7 +283,16 @@ def deleteSlivers(geni_slice, slivers):
     mani_rspec = geni_slice._manifest_rspec
     temp = mani_rspec.split('"')
     pi_name = temp[13]
-   # config.logger.info("<><><><> PI NAME = %s" % pi_name)
+    # config.logger.info("<><><><> PI NAME = %s" % pi_name)
+
+    # retrieve proper ip address based on pi name
+    pi_list = config.rpi_metadata
+    pidata = pi_list[pi_name]
+    public_ipv4 = pidata['public_ipv4']
+    #config.logger.info("public_ipv4 = %s" % public_ipv4)
+    
+    HOST = "pi@%s" % public_ipv4
+    #config.logger.info("HOST = %s" % HOST) 
 
     # We delete all the VMs before we delete the links.
     # Walk through the list of sliver_objects and create two list:
@@ -294,7 +312,7 @@ def deleteSlivers(geni_slice, slivers):
 
     # For each VM to be deleted, delete the VM and its associated network ports
     for vm in vms_to_be_deleted  :
-        success = _deleteVM(vm)
+        success = _deleteVM(vm, HOST)
         if success :
             vm.setAllocationState(constants.unallocated)
             vm.setOperationalState(constants.stopping)
@@ -304,7 +322,7 @@ def deleteSlivers(geni_slice, slivers):
 
     return return_val
 
-def _deleteVM(vm_object) :
+def _deleteVM(vm_object, HOST) :
     """
         Delete the OpenStack VM that corresponds to this vm_object.
         Delete the network ports associated with the VM
@@ -313,8 +331,8 @@ def _deleteVM(vm_object) :
     """
     return_val = True
 
-    # Delete the VM
-    ssh = subprocess.Popen(["ssh", "pi@128.89.91.174", "sudo", "./clean.sh"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Delete the "VM" by executing the clean script
+    ssh = subprocess.Popen(["ssh", "%s" % HOST, "sudo", "./temp/clean.sh"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = ssh.stdout.readlines()
     if result == []:
         error = ssh.stderr.readlines()
